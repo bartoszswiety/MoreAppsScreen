4fc19be05c90f8d8cda334b2c50e7931ed5373e4
Bartosz Swiety <>
2019-06-12 14:08:18 +0200

    Dir change

---
+++ Source/New Unity Project/Assets/MoreAppsPlugin/Example.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Example : MonoBehaviour
+{
+    public MoreAppsMaster moreAppsScreen;
+
+    void Start()
+    {
+        moreAppsScreen = MoreAppsMaster.Init("http://bartoszswiety.com/MoreApps.xml");
+    }
+
+    public void ShowMoreApps()
+    {
+
+        MoreAppsMaster.SimpleShow("http://bartoszswiety.com/MoreApps.xml");
+        //MoreAppsScreen.Show ();
+    }
+}
